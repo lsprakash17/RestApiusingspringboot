fb027988e5b204d6e62733d39a83755d138ff0c8
lsprakash17 <lspraksh17@gmail.com>
2023-06-15 18:54:31 +0530

    commit

---
+++ src/main/java/org/jsp/bootcrudrest/BootCrudRestApplication.java
@@ -0,0 +1,13 @@
+package org.jsp.bootcrudrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCrudRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCrudRestApplication.class, args);
+	}
+
+}
