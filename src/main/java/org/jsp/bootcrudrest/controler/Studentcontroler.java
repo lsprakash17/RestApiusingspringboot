dc4128a769e403c4c6b6398dae9f4589f6d9c7b3
lsprakash17 <lspraksh17@gmail.com>
2023-06-23 22:49:28 +0530

    commit

--- src/main/java/org/jsp/bootcrudrest/controler/Studentcontroler.java
+++ src/main/java/org/jsp/bootcrudrest/controler/Studentcontroler.java
@@ -3,6 +3,7 @@ package org.jsp.bootcrudrest.controler;
 import java.util.List;
 
 import org.jsp.bootcrudrest.dto.Student;
+import org.jsp.bootcrudrest.helper.ResponseStructure;
 import org.jsp.bootcrudrest.service.Studentservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -23,45 +24,45 @@ public class Studentcontroler
 	Studentservice service;
 	
  @PostMapping("students")
- public Student save(@RequestBody Student student)
+ public ResponseStructure<Student> save(@RequestBody Student student)
  {
 	 return  service.save(student);
  }
  @PostMapping("students/all")
- public List<Student> save(@RequestBody List<Student> student)
+ public ResponseStructure<List<Student>> save(@RequestBody List<Student> student)
  {
 	 return service.save(student);
  }
  
  @GetMapping("students/all")
- public  List<Student> fetch()
+ public  ResponseStructure<List<Student>> fetch()
  {
 	 return service.fetchall();
  }
  @GetMapping("students")
- public Student fetch(@RequestParam int id)
+ public ResponseStructure<Student> fetch(@RequestParam int id)
  {
 	 return service.fetch(id);
  }
  @DeleteMapping("students/sid")
- public void delete(@RequestParam int id)
+ public ResponseStructure<Student> delete(@RequestParam int id)
  {
-     service.delete(id);
+    return service.delete(id);
  }
  @GetMapping("students/{name}")
-public List<Student>fetchbyname(@PathVariable String name)
+public ResponseStructure<List<Student>> fetchbyname(@PathVariable String name)
 {
 	 return service.fetchbyname(name);
  
 }
  @GetMapping("student/{mobile}")
- public List<Student> fetchbymobile(@PathVariable long mobile)
+ public ResponseStructure<List<Student>> fetchbymobile(@PathVariable long mobile)
  
  {
 	 return service.fetchbymobile(mobile);
  }
  @GetMapping("students/results/{result}")
- public List<Student> Result(@PathVariable String result)
+ public ResponseStructure<List<Student>> Result(@PathVariable String result)
  {
 	 return service.Result(result);
  }
@@ -76,17 +77,17 @@ public List<Student>fetchbyname(@PathVariable String name)
 //	 return service.findByPhysicssGreater(marks);
 //}
  @GetMapping("students/{subject}/{marks}")
- public List<Student> findBySubject(@PathVariable String subject,@PathVariable int marks)
+ public ResponseStructure<List<Student>> findBySubject(@PathVariable String subject,@PathVariable int marks)
  {
 	 return service.findBySubject(subject,marks);
  }
  @GetMapping("studentss/{minmark}/{maxmark}")
- public List<Student> findByMinPhyAndMaxPhy(@PathVariable int minmark ,@PathVariable int maxmark)
+ public ResponseStructure<List<Student>> findByMinPhyAndMaxPhy(@PathVariable int minmark ,@PathVariable int maxmark)
  {
 	 return  service.findByMinPhyAndMaxPhy(minmark,maxmark);
  }
  @PutMapping("studentss/{id}")
- public Student edit(@PathVariable int id,@RequestBody Student student )
+ public ResponseStructure<Student> edit(@PathVariable int id,@RequestBody Student student )
  {
 	 return service.updateUser(id, student);
  }
--- src/main/java/org/jsp/bootcrudrest/helper/ResponseStructure.java
+++ src/main/java/org/jsp/bootcrudrest/helper/ResponseStructure.java
@@ -1,12 +1,16 @@
 package org.jsp.bootcrudrest.helper;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+
 import lombok.Data;
 
 
 @Data
-public class ResponseStructure<Student>
+public class ResponseStructure<T>
 {
 String message;
 int status;
-Student data;
+T data;
 }
